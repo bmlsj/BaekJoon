6dd31f2f29f1db0227b5b94056e0570878213c6e
bmlsj <gaga2353@gmail.com>
2022-01-13 00:36:26 +0900

    최대값

--- Python/TEST5/2562.py
+++ Python/TEST5/2562.py
@@ -5,5 +5,15 @@ num = []
 for i in range(9):
     num.append(int(input()))
 
-print(max(num))
-print(num.index(max(num))+1)
+# print(max(num))
+# print(num.index(max(num))+1)
+
+
+max = num[0]
+
+for i in range(1, 9):
+    if max < num[i]:
+        max = num[i]
+
+print(max)
+print(num.index(max)+1)
